10e3b94c996ee51473a7071912978c41be982c1b
Simon Willison <swillison@gmail.com>
2024-08-07 12:58:03 -0700

    Better display of logs in admin
    Closes #3
    
    Also adds semi-rendered base64 output, inspired by datasette-render-binary

--- django_http_debug/admin.py
+++ django_http_debug/admin.py
@@ -1,6 +1,12 @@
+import re
+import filetype
 from django.contrib import admin
 from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 from .models import DebugEndpoint, RequestLog
+
+sequence_re = re.compile(r"((?:\\x[0-9a-f]{2})+)")
+octet_re = re.compile(r"(\\x[0-9a-f]{2})")
 
 
 @admin.register(DebugEndpoint)
@@ -17,6 +23,7 @@ class RequestLogAdmin(admin.ModelAdmin):
         "endpoint",
         "method",
         "headers",
+        "body",
         "body_display",
         "is_base64",
         "timestamp",
@@ -35,10 +42,43 @@ class RequestLogAdmin(admin.ModelAdmin):
             return f"Binary data ({len(body)} bytes)"
         return body[:50] + ("..." if len(body) > 50 else "")
 
+    body_preview.short_description = "Body preview"
+
     def body_display(self, obj):
         body = obj.get_body()
-        if isinstance(body, bytes):
-            return format_html("<pre>Binary data ({} bytes)</pre>", len(body))
-        return format_html("<pre>{}</pre>", body)
+        if not isinstance(body, bytes):
+            return format_html("<pre>{}</pre>", body)
 
-    body_display.short_description = "Body"
+        # Attempt to guess filetype
+        suggestion = None
+        match = filetype.guess(body[:1000])
+        if match:
+            suggestion = "{} ({})".format(match.extension, match.mime)
+
+        encoded = repr(body)
+        # Ditch the b' and trailing '
+        if encoded.startswith("b'") and encoded.endswith("'"):
+            encoded = encoded[2:-1]
+
+        # Split it into sequences of octets and characters
+        chunks = sequence_re.split(encoded)
+        html = []
+        if suggestion:
+            html.append(
+                '<p style="margin-top: 0; font-family: monospace; font-size: 0.8em;">Suggestion: {}</p>'.format(
+                    suggestion
+                )
+            )
+        for chunk in chunks:
+            if sequence_re.match(chunk):
+                octets = octet_re.findall(chunk)
+                octets = [o[2:] for o in octets]
+                html.append(
+                    '<code style="color: #999; font-family: monospace">{}</code>'.format(
+                        " ".join(octets).upper()
+                    )
+                )
+            else:
+                html.append(chunk.replace("\\\\", "\\"))
+
+        return mark_safe(" ".join(html).strip().replace("\\r\\n", "<br>"))
